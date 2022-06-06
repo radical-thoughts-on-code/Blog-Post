b8cde4d048c3aacd1fe9f36bddba4da8a95ec53b
Eklipxe <93890931+Eklipxe@users.noreply.github.com>
2022-06-06 01:51:49 +0100

    Version 1 complete

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const _ = require("lodash")
 const app = express()
 
 app.use(express.static("public"))
@@ -7,7 +8,7 @@ app.set("view engine", "ejs")
 
 const posts = []
 
-// Placeholder Contents
+// Placeholder Contents for Home, about and contact pages
 const homeContent =
   "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea obcaecati repellendus a saepe mollitia, error natus sequi odio recusandae distinctio laboriosam, inventore eligendi, dignissimos laborum esse dolor aspernatur fugiat consectetur harum fuga? Doloribus, sed reiciendis placeat nostrum libero ex adipisci?"
 const aboutContent =
@@ -15,41 +16,64 @@ const aboutContent =
 const contactContent =
   "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae, beatae accusantium quia, impedit quam perferendis nulla fuga odio perspiciatis fugit consectetur mollitia libero placeat, quidem corrupti eos vel dolorum labore. Libero, esse. Corporis recusandae adipisci aut nobis dolore numquam soluta."
 
+//Get request to home route
 app.get("/", (req, res) => {
   res.render("home", { header: "Home", content: homeContent, newPosts: posts })
 })
 
+//Get request to about route
 app.get("/about", (req, res) => {
   res.render("about", { header: "About", content: aboutContent })
 })
 
+//Get contact to home route
 app.get("/contact", (req, res) => {
   res.render("contact", { header: "Contact", content: contactContent })
 })
 
+// Hidden route to compose page
 app.get("/compose", (req, res) => {
   res.render("compose", { header: "Compose" })
 })
 
+// Post route from compose page
 app.post("/compose", (req, res) => {
-  const tilte = req.body.postTitle
-  const content = req.body.postMsg
+  const title = req.body.title
+  const content = req.body.message
   const postObject = {
-    postTitle: "",
-    postMessage: "",
+    title: "",
+    message: "",
   }
 
-  if (tilte && content) {
-    postObject.postTitle = tilte
-    postObject.postMessage = content
+  /**Control flow for blog post content */
+  if (title && content) {
+    postObject.title = title
+    postObject.message = content
     posts.unshift(postObject)
-    res.redirect("/")
-  } else if (req.body.postMsg) {
-    postObject.postMessage = content
+    res.redirect("/") // Redirect user to hompage after blog post is updated
+  } else if (content) {
+    postObject.message = content
     posts.unshift(postObject)
-    res.redirect("/")
+    res.redirect("/") // Redirect user to hompage after blog post is updated
   }
 })
+
+app.get("/posts/:postName", (req, res) => {
+  const postTitle = _.lowerCase(req.params.postName)
+
+  const findPost = posts.find((post) => {
+    const title = _.lowerCase(post.title)
+    return title === postTitle
+  })
+  if(findPost){
+    res.render("posts", { header: findPost.title, content: findPost.message })
+  }else{
+    res.render('posts', {header: "SORRY!", content: "Please try another post"})
+    console.log( "Could not get post. \nReading: " + findPost)
+  }
+  
+})
+
 
 app.listen(2500, () => {
   console.log("App is running at port: 2500")
